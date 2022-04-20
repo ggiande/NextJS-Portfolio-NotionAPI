7e78329d7fafef1fbc7d9509f581ccee7b328dbd
ggiande <44848977+ggiande@users.noreply.github.com>
2022-04-20 02:14:34 -0700

    changed senseless text

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,5 +1,10 @@
 export default function Projects() {
   return(
-    <h1>Hello I am the projects page</h1>
+    <>
+      <h1>Hello I am the projects page</h1>
+      <h2>Alternatively, I can reuse the same code from Notion with a gallery of projects</h2>
+    </>
+
+    
   )
 }
